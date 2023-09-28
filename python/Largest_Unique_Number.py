e9986c9468ab5779ef80d499d4d0b108054d22cd
lry12138 <lry.ox.uk@gmail.com>
2023-09-28 16:59:35 +0100

    Create Largest_Unique_Number.py

---
+++ python/Largest_Unique_Number.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        c = collections.Counter(nums)
+
+        ans = - 1
+        for n in c:
+            if c[n] == 1:
+                if ans < n:
+                    ans = n
+        
+        return ans
